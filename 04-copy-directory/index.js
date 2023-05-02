231bc887b48792e3d3d9d25cff3dbf8d6396464f
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-05-03 00:23:52 +0300

    feat: completed fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,2 +1,32 @@
 const path = require('path');
 const fs = require('fs');
+
+const pathFiles = path.resolve(__dirname, 'files');
+const pathFilesCopy = path.resolve(__dirname, 'files-copy');
+
+function removeDir() {
+  fs.rm(pathFilesCopy, { recursive: true, force: true }, (err) => {
+    if (err) throw err;
+    copyDir();
+  });
+}
+
+function copyDir() {
+  fs.mkdir(pathFilesCopy, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(pathFiles, (err, files) => {
+    files.forEach((file) => {
+      fs.copyFile(
+        path.join(pathFiles, file),
+        path.join(pathFilesCopy, file),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    });
+  });
+}
+
+removeDir();
