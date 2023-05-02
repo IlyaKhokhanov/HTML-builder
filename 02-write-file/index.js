e4e702bf68f8e16d17634b20117ef3bc94cbe484
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-05-02 14:13:40 +0300

    feat: the second task is done

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const fs = require('fs');
+
+const { stdout, stdin } = process;
+
+const filePath = path.resolve(__dirname, 'text.txt');
+
+fs.readdir(path.resolve(__dirname), (err, files) => {
+  if (!files.includes('text.txt')) {
+    fs.open(filePath, 'w', (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
+stdout.write('Please write what you want to write to the file text.txt\n');
+stdin.on('data', (data) => {
+  fs.appendFile(filePath, data, (err) => {
+    if (err) throw err;
+    stdout.write('Please write what you want to write to the file text.txt\n');
+  });
+});
