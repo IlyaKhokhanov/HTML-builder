4d27602e415b372f044d39fcf3adefc49218709d
Ilya Khokhanov <112430875+IlyaKhokhanov@users.noreply.github.com>
2023-05-09 01:04:58 +0300

    fix: fix second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -15,6 +15,9 @@ fs.readdir(path.resolve(__dirname), (err, files) => {
 
 stdout.write('Please write what you want to write to the file text.txt\n');
 stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  }
   fs.appendFile(filePath, data, (err) => {
     if (err) throw err;
     stdout.write('Please write what you want to write to the file text.txt\n');
@@ -22,6 +25,9 @@ stdin.on('data', (data) => {
 });
 
 process.on('SIGINT', () => {
-  stdout.write('Thanks for check!');
   process.exit();
+});
+
+process.on('exit', () => {
+  stdout.write('Thanks for check!');
 });
